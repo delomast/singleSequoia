f2df48736df72dc1647ac7bc14c1fe8c18d23803
Tom Delomas <43458918+delomast@users.noreply.github.com>
2022-06-10 14:28:00 -0400

    add support for EFGLmh objects

--- R/toSequoiaInput.R
+++ R/toSequoiaInput.R
@@ -104,3 +104,77 @@ toSequoiaInput <- function(baselinePops, mixturePops, markerList, prefix = ""){
 	#output genotype file
 	write.table(genotypes, file=paste0(prefix, "genotypes.txt"), sep="\t", row.names=TRUE, col.names=FALSE, quote=FALSE)
 }
+
+#' Build Sequioa Input files for Tom's template script
+#'
+#' This interfaces with EFGLmh objects to build files expected by Tom's template Sequoia script
+#'   for single parent assignment (actual assignments, not simulations)
+#'
+#' @param x EFGLdata object with potential parents and offspring
+#' @param baselinePops names of pops with potential parents
+#' @param mixturePops names of pops with offspring
+#' @param markerList list of markers to use, if not provided the default is to use all
+#' @param prefix This is a string that will be used as the prefix for the two output files.
+#'   Default is to have no prefix.
+#' @return Writes files to the working directory that can be uploaded to the server and
+#'   used to run single parentage with Sequoia using Tom's template script
+#' @import EFGLmh
+#' @export
+
+toSequoiaInput_EFGLmh <- function(x, baselinePops, mixturePops, markerList = NULL, prefix = ""){
+	if(is.null(markerList)) markerList <- getLoci(x)
+	x <- x %>% removeLoci(lociRemove = getLoci(x)[!getLoci(x) %in% markerList])
+	#get parents
+	parent_IDs <- getInds(x, pops = baselinePops)
+
+	#get offspring
+	offspring_IDs <- getInds(x, pops = mixturePops)
+
+	scores_all <- x$genotypes[match(c(parent_IDs, offspring_IDs), x$genotypes$Ind),3:ncol(x$genotypes)]
+
+	#assign lifehistory fields
+	life_history <- data.frame(ID = c(parent_IDs, offspring_IDs),
+						  Sex = rep(1, length(c(parent_IDs, offspring_IDs))),
+						  BY = c(rep(1, length(parent_IDs)), rep(2, length(offspring_IDs))),
+						  stringsAsFactors = FALSE)
+
+	#check names for uniqueness
+	if(sum(table(life_history$ID) > 1) > 0) stop("The names of individuals are not unique.")
+
+	#output metadata file
+	write.table(life_history, file=paste0(prefix, "life_history.txt"), sep= "\t", row.names=FALSE, quote=FALSE)
+
+	#build genotype file
+	###define empty matrix with row names and column names
+	genotypes <- matrix(nrow=nrow(life_history), ncol=length(markerList),
+					dimnames=list(rows = c(parent_IDs, offspring_IDs), cols = markerList))
+	to_remove <- c()
+	###build genotype table
+	for (i in 1:ncol(genotypes)){
+		marker <- colnames(genotypes)[i]
+		a1 <- scores_all[[paste0(marker, ".A1")]]
+		a2 <- scores_all[[paste0(marker, ".A2")]]
+		Alleles <- unique(c(a1, a2))
+		Alleles <- Alleles[!is.na(Alleles)]
+		if (length(Alleles) == 0){
+			print(paste("Warning: all genotypes failed for locus", marker))
+			genotypes[,i] <- -9
+		}
+		if (length(Alleles) == 1){
+			print(paste("Warning: no variation in the population for locus", marker))
+			genotypes[,i] <- 2
+		}
+		if (length(Alleles) > 2){
+			print(paste0("Warning: more than two alleles found for locus ", marker, ". Removing this locus from the dataset."))
+			to_remove <- c(to_remove, marker)
+		}
+		else{		#if two alleles present
+			genotypes[,i] <- (a1 == Alleles[1]) + (a2 == Alleles[1]) # count of first allele
+			genotypes[is.na(genotypes[,i]),i] <- -9 # missing
+		}
+	}
+	genotypes <- genotypes[,!(colnames(genotypes) %in% to_remove)]
+	#output genotype file
+	write.table(genotypes, file=paste0(prefix, "genotypes.txt"), sep="\t", row.names=TRUE, col.names=FALSE, quote=FALSE)
+}
+
--- R/toSequoiaSimulation.R
+++ R/toSequoiaSimulation.R
@@ -67,3 +67,77 @@ toSequoiaSimulation <- function(baselinePops, markerList, prefix = ""){
 	write.table(parents, paste0(prefix, "parent_data.txt"), sep=",", quote = FALSE, row.names = FALSE, col.names = TRUE)
 	cat("\nA file with parent data and genotypes for use with the simulation script has been written to", paste0(prefix,"parent_data.txt"), "\n")
 }
+
+#' Build Sequioa Input files for \code{sequoiaSim}
+#'
+#' This interfaces with EFGLmh objects to build the parent_data_file expected by \code{sequoiaSim}
+#'
+#' @param x EFGLdata object with potential parents and offspring
+#' @param baselinePops names of pops with potential parents
+#' @param markerList list of markers to use
+#' @param prefix This is a string that will be used as the prefix for the output files.
+#'   Default is to have no prefix.
+#' @return Writes a file to the working directory that can be uploaded to the server and
+#'   used to run \code{sequoiaSim}
+#' @export
+
+toSequoiaSimulation_EFGLmh <- function(x, baselinePops, markerList = NULL, prefix = ""){
+	print("Note: this function assumes you have already removed all duplicate and failed individuals from your baseline populations.")
+	if(is.null(markerList)) markerList <- getLoci(x)
+	x <- x %>% removeLoci(lociRemove = getLoci(x)[!getLoci(x) %in% markerList])
+	parents <- matrix(nrow=0, ncol=(3 + length(markerList)))	#initiate emtpy matrix to fill with output
+	colnames(parents) <- c("Population", "Name", "Sex", markerList)
+	for (pop in baselinePops){	#iterate through populations
+		all_genos <- x$genotypes %>% filter(Pop == pop)
+		#build list of sex for each individual
+		sex_marker <- 0
+		for (i in 3:ncol(all_genos)){
+			if (sum(all_genos[,i] == "X") >= 1){
+				sex_marker <- i
+				break
+			}
+		}
+		if ("Gender" %in% colnames(x$metadata)){
+			pheno_sex <- x$metadata$Gender[match(all_genos$Ind, x$metadata$Ind)]
+			pheno_found <- TRUE
+		} else {
+			pheno_found <- FALSE
+		}
+		sex <- rep("U", nrow(all_genos))
+		if (sex_marker != 0){
+			sex[all_genos[[sex_marker]] == "X" & all_genos[[sex_marker + 1]] == "X"] <- "F"
+			sex[all_genos[[sex_marker]] == "Y" | all_genos[[sex_marker + 1]] == "Y"] <- "M"
+		}
+		if (pheno_found){
+			sex[sex == "U" & pheno_sex == "F"] <- "F"
+			sex[sex == "U" & pheno_sex == "M"] <- "M"
+		}
+		if(sum(sex == "U") > 0){
+			sex[sex == "U"] <- sample(c("M", "F"), sum(sex == "U"), replace = TRUE)
+		}
+
+		parents_temp <- data.frame(Population = pop, Name = all_genos$Ind, Sex = sex)
+		for(m in markerList){
+			a1 <- all_genos[[paste0(m, ".A1")]]
+			a1 <- all_genos[[paste0(m, ".A2")]]
+			parents_temp <- cbind(parents_temp, data.frame(m = paste0(a1, a2)))
+		}
+
+		parents_temp <- cbind(pop, all_genos$Ind, sex, all_genos[,markerList])
+		colnames(parents_temp) <- c("Population", "Name", "Sex", markerList)
+		parents <- rbind(parents, parents_temp)
+	}
+	#check names for NAs
+	if(sum(is.na(parents[,2]), parents[,2] == "") > 0){
+		stop("Error: one or more individuals in your baseline does not have a name.")
+	}
+	#check names for uniqueness
+	tempTable <- table(parents[,2])
+	if(sum(tempTable > 1) > 0){ # if not unique
+		stop("The names of individuals are not unique across populations.")
+	}
+	#output file to upload to server
+	write.table(parents, paste0(prefix, "parent_data.txt"), sep=",", quote = FALSE, row.names = FALSE, col.names = TRUE)
+	cat("\nA file with parent data and genotypes for use with the simulation script has been written to", paste0(prefix,"parent_data.txt"), "\n")
+}
+
